14f69aca65f87fcb6d74a04fff91506698350e60
Solorio17 <jasonsoloriom@gmail.com>
2017-09-17 15:41:00 -0600

    Adding a route.get Adding articles

--- routes/manage.js
+++ routes/manage.js
@@ -5,4 +5,8 @@ router.get("/", (req, res, next) => {
   res.send("MANAGE");
 });
 
+router.get("/articles/add", (req, res, next) => {
+  res.render("add_article", {title: "Create Article"})
+});
+
 module.exports = router;
